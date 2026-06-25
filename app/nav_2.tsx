984196c73f5025c86277b881008b28822ec14ec2
mahmoudkhateep7-arch <mahmoudkhateep7@gmail.com>
2026-06-25 21:25:59 +0300

    added idx to hide an element

--- app/nav_2.tsx
+++ app/nav_2.tsx
@@ -4,9 +4,9 @@ import Nav_link_C from "./nav_link"
 export default function Nav_2_C() {
   return (
     <div className={`gap-5 sm:flex hidden`}>
-      {navLinks.map(({ label, bg, borders, textColor }) => {
+      {navLinks.map(({ label, bg, borders, textColor }, idx) => {
         return (
-          <Nav_link_C textColor={textColor} bg={bg} borders={borders} key={label} label={label}></Nav_link_C>
+          <Nav_link_C idx={idx} textColor={textColor} bg={bg} borders={borders} key={label} label={label}></Nav_link_C>
         )
       })}
     </div>
